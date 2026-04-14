4fb671ebf64d90a093dbf60b996d090bb2f00d88
najustar <oliveiraannajulia529@gmail.com>
2026-04-14 16:24:36 +0000

    csv e pdf

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useMemo, useRef } from "react";
 import { TaskForm, Task } from "./components/TaskForm";
 import { TaskList } from "./components/TaskList";
 import { FilterBar } from "./components/FilterBar";
+import { exportToCSV, exportToPDF } from './exporta';
 
 type SavedTask = Partial<Task> & {
   titulo?: unknown;
